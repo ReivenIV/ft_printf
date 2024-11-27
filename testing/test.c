e241c685fee4230ac8b7764e922201fe055e0f31
ReivenIV <fwebe-ir@student.42perpignan.fr>
2024-11-27 16:40:19 +0100

    first test printf

---
+++ testing/test.c
@@ -0,0 +1,68 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return ;
+}
+
+void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = n * -1;
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+	}
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+}
+
+void	test_variadic(const char *str, ...)
+{
+	int i;
+	
+
+	i = 0;
+	va_list	args;
+	va_start(args, str);
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '%' && str[i + 1] == 'i')
+		{
+			int num = va_arg(args, int);
+			ft_putnbr(num);
+			i++;
+		}
+		else if (str[i] == '%' && str[i + 1] == 'c')
+		{
+			char test = (char)va_arg(args, int);
+			ft_putchar(test);
+			i++;
+		}
+		else {
+			ft_putchar(str[i]);
+		}
+		i++;
+	}
+	va_end(args);
+}
+
+int	main(void)
+{
+	test_variadic("test %i %i %i %i %i %c", 50,40,30,20,10,'H');
+	return (0);
+}
