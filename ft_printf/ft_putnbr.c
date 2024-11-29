2dc512bc6dbfb5265da24ddb7760582bf6233e45
ReivenIV <runyalen88@duck.com>
2024-11-29 14:07:37 +0200

    ft_putnbr created

---
+++ ft_printf/ft_putnbr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwebe-ir <fwebe-ir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 13:34:55 by fwebe-ir          #+#    #+#             */
+/*   Updated: 2024/11/29 13:34:55 by fwebe-ir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+//TODO should return double instead of int like that you handle ints and doubles.
+void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = n * -1;
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+	}
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+	return ;
+}
