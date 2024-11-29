bdc349e1398c51d0f3af36fbd8f0872792b48560
ReivenIV <runyalen88@duck.com>
2024-11-29 14:55:07 +0100

    header indentation

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fwebe-ir <fwebe-ir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 16:36:22 by fwebe-ir          #+#    #+#             */
-/*   Updated: 2024/11/27 16:45:04 by fwebe-ir         ###   ########.fr       */
+/*   Updated: 2024/11/29 14:47:47 by rita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 
 # include <unistd.h>
 # include <stdarg.h>
-# include <stdio.h> //! to erase
+# include <stdio.h> 
 
-int ft_printf(const char *src, ...);
-int	ft_putnbr(int n);
-int count_digits(int n);
+int 		ft_printf(const char *src, ...);
+void		ft_putnbr(int n);
+int	    	count_digits(int n);
+void	    ft_putchar(char c);
 
 #endif
