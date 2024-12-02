cb82099b2be3bb336802be7437082397cb33dd72
ReivenIV <runyalen88@duck.com>
2024-12-02 10:20:02 +0100

    headers updated

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 16:36:22 by fwebe-ir          #+#    #+#             */
-/*   Updated: 2024/11/30 12:22:49 by rita             ###   ########.fr       */
+/*   Updated: 2024/12/02 10:18:47 by rita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 # include <stdarg.h>
 # include <stdio.h>
 
-int			ft_printf(const char *src, ...);
-void		ft_putnbr(int n);
-int			count_digits(int n);
-void		ft_putchar(char c);
-int			ft_strlen(char *str);
-int			ft_putstr(char *str);
+int		ft_printf(const char *src, ...);
+void	ft_putnbr(int n);
+int		count_digits(int n);
+int		ft_putchar(char c);
+int		ft_strlen(char *str);
+int		ft_putstr(char *str);
+int		ft_print_int(int n);
 
 #endif
