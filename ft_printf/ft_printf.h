63b724f764bd6cb15ee4776ea737e71c230db346
ReivenIV <fwebe-ir@student.42perpignan.fr>
2024-12-03 17:01:37 +0100

    ft_print_str created

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,12 +6,12 @@
 /*   By: fwebe-ir <fwebe-ir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 15:48:38 by fwebe-ir          #+#    #+#             */
-/*   Updated: 2024/12/03 14:38:02 by fwebe-ir         ###   ########.fr       */
+/*   Updated: 2024/12/03 16:52:27 by fwebe-ir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <unistd.h>
 # include <stdarg.h>
@@ -23,11 +23,10 @@ void	ft_putnbr(long int n);
 int		count_digits(long int n);
 int		ft_putchar(char c);
 int		ft_strlen(char *str);
-int		ft_putstr(char *str);
-int		ft_putnbr_hex(unsigned long n, int n_len);
+int		ft_print_str(char *str);
 int		ft_print_p(void *p);
-int		print_hex_handler(char type, int n_len, unsigned long n);
-int		ft_putnbr_hex_lowercasse(unsigned long n, int n_len);
-int		ft_putnbr_hex_uppercase(unsigned long n, int n_len);
+int		ft_print_hex_handler(char type, long n);
+int		ft_putnbr_hex_lowercasse(long n, int n_len);
+int		ft_putnbr_hex_uppercase(long n, int n_len);
 
 #endif
