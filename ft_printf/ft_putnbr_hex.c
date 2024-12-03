3d16ac2408f471c30ba0ac21b448f2c860bde5a5
ReivenIV <fwebe-ir@student.42perpignan.fr>
2024-12-03 13:54:21 +0100

    ft_putnbr_hex.c created

--- ft_printf/ft_putnbr_hex.c
+++ ft_printf/ft_putnbr_hex.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_prt.c                                     :+:      :+:    :+:   */
+/*   ft_putnbr_hex.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fwebe-ir <fwebe-ir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/03 09:56:01 by fwebe-ir          #+#    #+#             */
-/*   Updated: 2024/12/03 13:53:47 by fwebe-ir         ###   ########.fr       */
+/*   Created: 2024/12/03 13:54:00 by fwebe-ir          #+#    #+#             */
+/*   Updated: 2024/12/03 13:54:02 by fwebe-ir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
-
-int	ft_print_p(void *p)
+// will transform any base 10 unsigned int into hex.
+int	ft_putnbr_hex(unsigned long n, int n_len)
 {
-	unsigned long	adrs;
-	int				len;
+	char	*hex_digits;
 
-	adrs = (unsigned long)p;
-	ft_putstr("0x");
-	len = ft_putnbr_hex(adrs, 0);
-	return (len + 2);
+	hex_digits = "0123456789abcdef";
+	if (n >= 16)
+		n_len = ft_putnbr_hex(n / 16, n_len + 1);
+	else
+		n += 1;
+	ft_putchar(hex_digits[n % 16]);
+	return (n_len);
 }
